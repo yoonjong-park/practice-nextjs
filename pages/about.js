781b81c678e23b1ad36fe59b78f749aa4f9fc5fc
yoonjong.park <>
2021-09-24 10:08:23 +0900

    Initial Commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const About = () => {
+  return (
+    <div>
+      <Link href="/">
+        <a>Move Main Page</a>
+      </Link>
+    </div>
+  );
+};
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
-export default function Home() {
+const Index = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -17,11 +18,15 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
+          <Link href="/about">
+            <a>Move About Page</a>
+          </Link>
+
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
@@ -58,12 +63,14 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
+
+export default Index;
